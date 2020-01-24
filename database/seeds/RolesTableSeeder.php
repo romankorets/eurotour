8726a4ecfc97a46cc6e2f434976fc92d4cf5f447
romankorets <RKorets.98@gmail.com>
2020-01-24 17:41:33 +0000

    add RolesTableSeeder

---
+++ database/seeds/RolesTableSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class RolesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $author = \App\Role::create([
+            'name' => 'User',
+            'slug' => 'user',
+            'permissions' => [
+                'create-comment' => true
+            ]
+        ]);
+
+        $editor = \App\Role::create([
+            'name' => 'Admin',
+            'slug' => 'admin',
+            'permissions' => [
+                'create-place' => true,
+                'edit-place' => true,
+                'delete-place' => true,
+                'create-tour' => true,
+                'edit-tour' => true,
+                'delete-tour' => true,
+                'create-comment' => true,
+                'disable-comment' => true,
+                'edit-comment' => true
+            ]
+        ]);
+    }
+}
