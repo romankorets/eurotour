947b6c481e783df70f6c1be727cff01ed621d762
romankorets <RKorets.98@gmail.com>
2020-01-24 11:55:31 +0000

    add methods hasRole and hasAccess to User model

--- app/User.php
+++ app/User.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use App\Role;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -36,4 +37,21 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function roles(){
+        return $this->belongsToMany(Role::class, 'roles_user', 'userId', 'roleId');
+    }
+
+    public function hasRole($roleSlug) : bool {
+        return $this->roles()->where('slug', $roleSlug)->count() == 1;
+    }
+
+    public function hasAccess(array $permissions) : bool {
+        foreach ($this->roles as $role){
+            if($role->hasAccess($permissions)){
+                return true;
+            }
+        }
+        return false;
+    }
 }
